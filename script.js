40ee3515a57b89a34568cdb8ded13dc165278508
kilamieaz <im.sultaan05@gmail.com>
2018-09-03 09:42:36 +0700

    data types

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 var firstName = 'Sultan';
 console.log(firstName);
+
+var age = 28;
+console.log(age)
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
