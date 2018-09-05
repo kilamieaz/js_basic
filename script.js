756a1b679c149e253fb176f8d98c7adecd2da071
kilamieaz <im.sultaan05@gmail.com>
2018-09-05 07:55:22 +0700

    switch statements

--- script.js
+++ script.js
@@ -163,3 +163,34 @@ if (age >= 18){
 } else {
     var drink = 'juice';
 }
+
+// Switch Statement
+var job = 'teacher';
+switch (job) {
+    case 'teacher':
+        console.log(firstName + ' teaches kids how to code.');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an uber in Jakarta.');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs beautiful websites.');
+        break;
+    default:
+        console.log(firstName + ' does something else.');
+}
+
+age = 10;
+switch (true) {
+    case age < 13 :
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 12 && age < 20 :
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age >= 20 && age < 30 :
+        console.log(firstName + ' is a young man.');
+        break;
+    default:
+        console.log(firstName + ' is a man.');
+}
