52992a089ad10193511bbdf8224aef1e76c03b03
kilamieaz <im.sultaan05@gmail.com>
2018-09-13 08:11:13 +0700

    hoisting

--- script.js
+++ script.js
@@ -439,72 +439,98 @@ console.log(typeof x);
 // }
 
 // example 
-var sultan = {
-    fullName: 'Sultan imam',
-    bills: [124, 48, 268, 180, 42],
-    calcTips: function() {
-        this.tips = [];
-        this.finalValues = [];
-        for (var i = 0; i < this.bills.length; i++){
-            // Determine percentage based on tipping rules
-            var percentage;
-            var bill = this.bills[i];
-            if (bill < 50) {
-                percentage = .2;
-            } else if(bill >= 50 && bill < 200) {
-                percentage = .15;
-            } else {
-                percentage = .1;
-            }
-            // Add results to the corresponing arrays
-            this.tips[i] = bill * percentage;
-            this.finalValues[i] = bill + bill * percentage;``
-        }
-    }
+// var sultan = {
+//     fullName: 'Sultan imam',
+//     bills: [124, 48, 268, 180, 42],
+//     calcTips: function() {
+//         this.tips = [];
+//         this.finalValues = [];
+//         for (var i = 0; i < this.bills.length; i++){
+//             // Determine percentage based on tipping rules
+//             var percentage;
+//             var bill = this.bills[i];
+//             if (bill < 50) {
+//                 percentage = .2;
+//             } else if(bill >= 50 && bill < 200) {
+//                 percentage = .15;
+//             } else {
+//                 percentage = .1;
+//             }
+//             // Add results to the corresponing arrays
+//             this.tips[i] = bill * percentage;
+//             this.finalValues[i] = bill + bill * percentage;``
+//         }
+//     }
+// }
+
+// var mark = {
+//     fullName: 'Mark bob',
+//     bills: [77, 375, 110, 45, 42],
+//     calcTips: function() {
+//         this.tips = [];
+//         this.finalValues = [];
+//         for (var i = 0; i < this.bills.length; i++){
+//             // Determine percentage based on tipping rules
+//             var percentage;
+//             var bill = this.bills[i];
+//             if (bill < 100) {
+//                 percentage = .2;
+//             } else if(bill >= 100 && bill <= 300) {
+//                 percentage = .1;
+//             } else {
+//                 percentage = .25;
+//             }
+//             // Add results to the corresponing arrays
+//             this.tips[i] = bill * percentage;
+//             this.finalValues[i] = bill + bill * percentage;``
+//         }
+//     }
+// }
+
+// function calcAverage(tips) {
+//     var sum = 0;
+//     for (var i = 0; i < tips.length; i++) {
+//         sum = sum + tips[i];
+//     }
+//     return sum / tips.length;
+// }
+// // 2, 6, 4 -> 0 / 2 / 8 / 12
+
+// // Do the calculations
+// sultan.calcTips();
+// mark.calcTips();
+// sultan.average = calcAverage(sultan.tips);
+// mark.average   = calcAverage(mark.tips);
+// console.log(sultan, mark);
+
+// if (sultan.average > mark.average) {
+//     console.log(sultan.fullName + '\'s family pays higher tips, with an average af $' + sultan.average);
+// } else if (mark.average > john.average) {
+//     console.log(mark.fullName + '\'s family pays higher tips, with an average of $' + marka.average)
+// }
+
+
+// hoisting
+calculateAge(1998);
+
+function calculateAge(year) {
+    console.log(2018 - year);
 }
 
-var mark = {
-    fullName: 'Mark bob',
-    bills: [77, 375, 110, 45, 42],
-    calcTips: function() {
-        this.tips = [];
-        this.finalValues = [];
-        for (var i = 0; i < this.bills.length; i++){
-            // Determine percentage based on tipping rules
-            var percentage;
-            var bill = this.bills[i];
-            if (bill < 100) {
-                percentage = .2;
-            } else if(bill >= 100 && bill <= 300) {
-                percentage = .1;
-            } else {
-                percentage = .25;
-            }
-            // Add results to the corresponing arrays
-            this.tips[i] = bill * percentage;
-            this.finalValues[i] = bill + bill * percentage;``
-        }
-    }
+// retirement(1956) = error
+var retirement = function(year) {
+    console.log(65 - (2018 - year));
 }
 
-function calcAverage(tips) {
-    var sum = 0;
-    for (var i = 0; i < tips.length; i++) {
-        sum = sum + tips[i];
-    }
-    return sum / tips.length;
-}
-// 2, 6, 4 -> 0 / 2 / 8 / 12
+// variables
 
-// Do the calculations
-sultan.calcTips();
-mark.calcTips();
-sultan.average = calcAverage(sultan.tips);
-mark.average   = calcAverage(mark.tips);
-console.log(sultan, mark);
+console.log(age); // undefined
+var age = 20;
+console.log(age);
 
-if (sultan.average > mark.average) {
-    console.log(sultan.fullName + '\'s family pays higher tips, with an average af $' + sultan.average);
-} else if (mark.average > john.average) {
-    console.log(mark.fullName + '\'s family pays higher tips, with an average of $' + marka.average)
+function foo() {
+    var age = 65;
+    console.log(age);  
 }
+foo();// 65
+console.log(age); // 20
