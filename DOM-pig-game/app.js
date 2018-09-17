927ca141b9ceed073c0f66b0f5bda09944049899
kilamieaz <im.sultaan05@gmail.com>
2018-09-17 20:24:27 +0700

    costume winning score

--- DOM-pig-game/app.js
+++ DOM-pig-game/app.js
@@ -44,8 +44,16 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         scores[activePlayer] += roundScore;
         // Update the UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        // costume score
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+        if (input) {
+            winningScore = input;
+        } else {
+            winningScore = 100;
+        }
         // Check if player won the game
-        if (scores[activePlayer] >= 20){
+        if (scores[activePlayer] >= winningScore){
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-'+ activePlayer + '-panel').classList.add('winner');
