c6c0af85d1de246225b70c2a77edfbd980700779
kilamieaz <im.sultaan05@gmail.com>
2018-09-19 13:21:30 +0700

    closures

--- Objects/script.js
+++ Objects/script.js
@@ -164,3 +164,39 @@ interviewQuestion('teacher')('Mark');
     console.log(score >= 5 - goodLuck);
 })(5);
  */
+
+ // Closures
+
+ function retirement(retirementAge) {
+     var a = ' years left until retirement.'
+     return function (yearOfBirth) {
+         var age = 2018 - yearOfBirth;
+         console.log((retirementAge - age) + a);
+     }
+ }
+
+ var retirementUS = retirement(66);
+ var retirementGermany = retirement(65);
+ var retirementIceland = retirement(67);
+ retirementUS(1998);
+ retirementGermany(1998);
+ retirementIceland(1998);
+
+function interviewQuestion(job) {
+    return function (name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?') 
+        } else {
+            console.log('Hello ' + name + ', what do yo do?');
+        }
+    }
+}
+
+var interviewDesigner = interviewQuestion('designer');
+var interviewTeacher = interviewQuestion('teacher');
+var interviewAnother = interviewQuestion('another');
+interviewDesigner('sultan')
+interviewTeacher('sultan')
+interviewAnother('sultan')
