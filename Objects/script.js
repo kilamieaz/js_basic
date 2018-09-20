fddd6a1293ce73b813db317ced4f3ef2ba49e8ed
kilamieaz <im.sultaan05@gmail.com>
2018-09-20 08:25:32 +0700

    bind method

--- Objects/script.js
+++ Objects/script.js
@@ -226,4 +226,34 @@ var emily = {
 sultan.presentation.call(emily, 'formal', 'afternoon')
 
 // apply method
-sultan.presentation.apply(emily,['friendly', 'afternoon']);
+// sultan.presentation.apply(emily,['friendly', 'afternoon']);
+
+// bind method
+// carrying = we create function based on another function with some preset parameter
+var sultanFriendly = sultan.presentation.bind(sultan, 'friendly');
+sultanFriendly('morning');
+sultanFriendly('night');
+
+
+var years = [1998, 1965, 1942, 2003, 2012];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++){
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2018 - el;
+}
+
+function isFullAge(limit, el) {
+    return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages)
+console.log(fullJapan)
