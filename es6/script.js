8571ccac2e39a23bfefd7266d1e6bac4c326543a
kilamieaz <im.sultaan05@gmail.com>
2018-10-02 21:36:00 +0700

    ES6, Maps

--- es6/script.js
+++ es6/script.js
@@ -340,7 +340,7 @@ isFullAge6(16, 1998, 2003, 2002);
 */
 
 // Lecture: default parameters
-
+/**
 // ES5
 // function SmithPerson(firstname, yearOfBirth, lastName, nationality) {
 //     lastName == undefined ? lastName = 'smith' : lastName = lastName;
@@ -361,3 +361,38 @@ function SmithPerson(firstname, yearOfBirth, lastName = 'Smith', nationality = '
 
 var sultan = new SmithPerson('sultan', 1998);
 var emily = new SmithPerson('emily', 1994, 'diaz', 'spanish');
+*/
+
+// Lecture: Maps
+
+const question = new Map();
+question.set('question', 'What is the official name of the lateste mojor JavaScript version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer :D');
+question.set(false, 'Wrong, please try again!');
+
+console.log(question.get(question.get('question')));
+console.log(question.size);
+question.delete(4);
+
+if (question.has(4)) {
+    // question.delete(4);
+    console.log('Answer 4 is here');
+}
+
+// question.clear();
+
+// question.forEach((val, key) => console.log(`this is ${key}, and it's set ${val}`));
+
+for (let [key, value] of question.entries()) {
+    if (typeof(key) === 'number') {
+        console.log(`Answer ${key}: ${value}`);
+    }
+}
+
+const ans = parseInt(prompt('Write the correct answer'));
+console.log(question.get(ans === question.get('correct')));
